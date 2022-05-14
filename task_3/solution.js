1ca8d7c026bc188e35d5bbfb56ebe36c583d313c
svanonline <101625280+svanonline@users.noreply.github.com>
2022-05-14 12:42:52 +0300

    Update solution.js

--- task_3/solution.js
+++ task_3/solution.js
@@ -1,24 +1,6 @@
 function renderCartItem(item) {
     const product = item;
 
-    // Задание №3.1. Формирование строки корзины
-
-    //let products = [
-        //{id: 1, title: 'Эчпочмак', count: 2, price: '55.00'},
-        //{id: 2, title: 'Пицца', count: 5, price: '500.00'}];
-    
-    
-                
-    // например, {id: 1, title: 'Пицца', count: 5, price: '500.00'}, где
-    // id - идентификатор
-    // title - наименование
-    // price - цена
-    // count - количество
-
-    // сформируйте строку itemCountText вида:
-    // количество × цена ₽ = стоимость ₽
-    // например,
-    // 5 × 500.00 ₽ = 2500 ₽
     count = product.count;
     price = product.price;
 
