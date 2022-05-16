7688039c2b7d254bbc2569f965bb7c712d0d51e9
svanonline <101625280+svanonline@users.noreply.github.com>
2022-05-16 14:55:44 +0300

    Update solution.js

--- task_4/solution.js
+++ task_4/solution.js
@@ -15,9 +15,8 @@ function parseProducts(json) {
 
 function renderProductsCards(json) {
     clearProducts();
-    products = parseProducts(json)
-    console.log(parseProducts(json))
-    length = products.length
+    products = parseProducts(json);
+    length = products.length;
     for (let i = 0; i < length; i += 1) {
         addProduct(products[i])
      }
