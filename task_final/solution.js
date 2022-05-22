2f7d329a68dfbcf65740376cfad37702ff1505b2
svanonline <101625280+svanonline@users.noreply.github.com>
2022-05-22 15:35:11 +0300

    Final proj

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,18 +34,19 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {goods: [], order: [], client: []};
 
     let countOfGoods = goods.length;
 
     for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+        data.goods.push(goods[i]);
     }
+    
 
     data.order.address = address;
     data.order.sum = name + phone + address + goods + sum;
 
-    data.client = 'Иван';
+    data.client = name;
 
     let jsonData = JSON.stringify(data);
 
