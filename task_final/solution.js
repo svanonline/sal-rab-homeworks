4d85d5c5e2893f3ff92cb8192f5465245393a1c4
svanonline <101625280+svanonline@users.noreply.github.com>
2022-05-24 16:46:40 +0300

    final project all working

--- task_final/solution.js
+++ task_final/solution.js
@@ -42,10 +42,10 @@ function sendRequest(name, phone, address, goods, sum) {
         data.goods.push({"title": goods[i].title, "count": goods[i].count});
     }
 
-    data.address = JSON.stringify('ул. ' + address.street + ', дом ' + address.house + ', ' + address.entrance + ' подъезд, '  + address.floor + ' этаж' + ', кв ' + address.flat);
-    data.sum = sum;
-
     data.client = name + ' ' + phone;
+
+    data.order = ({address: 'ул. ' + address.street + ', дом ' + address.house + ', ' + address.entrance + ' подъезд, '  + address.floor + ' этаж' + ', кв ' + address.flat});
+    data.order.sum = sum;
 
     let jsonData = JSON.stringify({data});
 
